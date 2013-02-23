4a8d9d7457916d5adefd1a29f01db24ea7c27594
ralph <ralph@pencilmedia.com>
2013-02-22 23:54:23 -0800

    PM Website

---
+++ global/includes/foot/foot_global.inc.php
@@ -0,0 +1,35 @@
+<!--
+<a href="javascript:theme.readIt('pmcookie1')" class="page">Read cookie 1</a><br>
+-->
+<footer>
+    <div id="styleSwitcher">
+        <div class="colorBox beige" title="">&nbsp;</div>
+        <div class="colorBox silver" title="silver">&nbsp;</div>
+        <div class="colorBox orange" title="orange">&nbsp;</div>
+        <div class="colorBox lavendar" title="lavendar">&nbsp;</div>
+        <div class="colorBox yellow" title="yellow">&nbsp;</div>
+        <div class="colorBox blue" title="blue">&nbsp;</div>
+        <div class="colorBox pink" title="pink">&nbsp;</div>
+        <div class="colorBox red" title="red">&nbsp;</div>
+        <div class="colorBox brightgreen" title="brightgreen">&nbsp;</div>
+        <div class="colorBox green" title="green">&nbsp;</div>
+        <span id="clearTheme"><span>Clear</span></span>
+    </div>
+    <ul>
+        <li><?php echo copyright ?> </li>
+        <li><a href="<?php echo pathToUX ?>"><?php echo uxHead ?></a></li>
+    </ul>
+    <div id="footbar">&nbsp;</div>
+</footer>
+
+<!-- GOOGLE ANALYTICS -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-15047911-1");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
