f5363428f0d53604570fabb6ecd668e7550ebf2e
ralph <ralph@pencilmedia.com>
2013-02-23 22:37:41 -0800

    bootstrap framework additions

---
+++ assets/inc/socialbar.inc.php
@@ -0,0 +1,9 @@
+<div id="social-bar">
+    <a id="ico_home" href="http://www.pencilmedia.com" title="Back to Homepage" class="pngfix">Back to Homepage</a>
+    <a id="ico_facebook" href="http://www.facebook.com/share.php?u=http://www.pencilmedia.com" target="_blank" title="Share us on Facebook" class="pngfix">Share on Facebook</a>
+    <a id="ico_linkedin" href="http://www.linkedin.com/profile/view?id=6016434&trk=tab_pro" target="_blank" title="View my LinkedIn Profile" class="pngfix">View my LinkedIn Profile</a>
+</div>
+<div id="overlay-background" class="close"></div>
+<div class="apple_overlay" id="overlay">
+	<div class="contentWrap"></div>
+</div>
