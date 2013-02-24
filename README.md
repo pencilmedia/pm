2d51934402d30c58adaa08a5f88ae018e747ab0c
ralph <ralph@pencilmedia.com>
2013-02-23 23:11:32 -0800

    deleting junk

--- README.md
+++
@@ -1,4 +0,0 @@
-pm
-==
-
-PM Website
--- pm/assets/inc/nav/labs.inc.php
+++
@@ -1,5 +0,0 @@
-		<nav class="subnav">
-            <ul>
-                <li <?php if ($subPage=="labsHome") echo " id=\"selected\""; ?>><a href="index.php" id="nav_web"><?php echo labsProj ?></a></li>
-            </ul>
-        </nav>
